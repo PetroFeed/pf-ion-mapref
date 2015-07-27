467e59ae103c77dcfe394ab6fb151ecd666cce2b
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2015-07-27 15:42:44 -0300

    Create angular directive `pf-ion-mapref`.

--- dist/pf-ion-mapref.js
+++ dist/pf-ion-mapref.js
@@ -1,5 +1,20 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-require('./mapref');
+var MapRef = require('./mapref');
+
+angular.module('pf-ion-mapref', [])
+.directive('pfIonMapref', function pfIonMapref () {
+  var directive = {
+    restrict: 'A',
+    scope: { coordinates: '=pfIonMapref' }
+  };
+
+  directive.link = function link (scope, element) {
+    var href = MapRef.forCoordinates(scope.coordinates);
+    element.attr('href', href);
+  };
+
+  return directive;
+});
 
 },{"./mapref":2}],2:[function(require,module,exports){
 exports.forCoordinates = function forCoordinates (latitude, longitude) {
