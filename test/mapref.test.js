91258f39cd71b3ef4ac01a44d3b61614499ea2d1
Tim Uruski <tim@petrofeed.com>
2015-08-12 17:17:06 -0600

    Refactored to make more reusable

--- test/mapref.test.js
+++ test/mapref.test.js
@@ -1,42 +1,52 @@
 var test = require('tape');
 var MapRef = require('../lib/mapref');
 
-test('MapRef.forCoordinates when ionic is not found', function (t) {
+test('MapRef.forCoordinates expands http protocol to Google Maps', function (t) {
+  t.equal(MapRef.forCoordinates('http', 10, 20), 'http://maps.google.com/?q=10,20');
+  t.end();
+});
+
+test('MapRef.forCoordinates adds :// to other protocols', function (t) {
+  t.equal(MapRef.forCoordinates('gopher', 10, 20), 'gopher://?q=10,20');
+  t.end();
+});
+
+test('MapRef.resolveMapsProtocolByPlatform when ionic is not found', function (t) {
   var oWarn = console.warn;
   console.warn = function warn (message) {
     t.equal(message, '`ionic` not found!');
     t.end(); console.warn = oWarn;
   };
 
-  t.equal(MapRef.forCoordinates(10, 20), 'http://maps.google.com/?q=10,20');
+  t.equal(MapRef.resolveMapsProtocolByPlatform(), 'http');
 });
 
-test('MapRef.forCoordinates on Android', function (t) {
+test('MapRef.resolveMapsProtocolByPlatform on Android', function (t) {
   ionic = { Platform: {
     isAndroid: function () { return true; },
     isIOS: function () { return false; }
   }};
 
-  t.equal(MapRef.forCoordinates(10, 20), 'geo://?q=10,20');
+  t.equal(MapRef.resolveMapsProtocolByPlatform(), 'geo');
   t.end();
 });
 
-test('MapRef.forCoordinates on iOS', function (t) {
+test('MapRef.resolveMapsProtocolByPlatform on iOS', function (t) {
   ionic = { Platform: {
     isAndroid: function () { return false; },
     isIOS: function () { return true; }
   }};
 
-  t.equal(MapRef.forCoordinates(10, 20), 'maps://?q=10,20');
+  t.equal(MapRef.resolveMapsProtocolByPlatform(), 'maps');
   t.end();
 });
 
-test('MapRef.forCoordinates otherwise', function (t) {
+test('MapRef.resolveMapsProtocolByPlatform otherwise', function (t) {
   ionic = { Platform: {
     isAndroid: function () { return false; },
     isIOS: function () { return false; }
   }};
 
-  t.equal(MapRef.forCoordinates(10, 20), 'http://maps.google.com/?q=10,20');
+  t.equal(MapRef.resolveMapsProtocolByPlatform(), 'http');
   t.end();
 });
